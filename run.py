850d029c1379f1120f1775964c289382e68aa297
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-02-26 23:49:10 +0000

    update docstring of get_sales_data function

--- run.py
+++ run.py
@@ -16,6 +16,9 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input form the user
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
